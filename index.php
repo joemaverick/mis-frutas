71c53d0a6cf32ea117abf66ac3e4347064727fe8
joemaverick <yehulugeta@gmail.com>
2019-02-13 03:39:13 -0800

    Header layout completed for Desktop and Mobile View

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mis Frutas</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1 "
+	<!-- link to uikit css -->
+	<link rel="stylesheet" type="text/css" href="/uikit/css/uikit.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/uikit.min.css">
+
+	<!-- link to google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<!-- link to custom css -->
+	<link rel="style/css" type="text/css" href="/css/custom.css">
+</head>
+<body>
+
+	<!-- Start of Header -->
+	<nav class="uk-navbar">
+		<div class="uk-container uk-container-center">
+		    <a href="" class="uk-navbar-brand uk-hidden-small"><img src="images/circlelogo.jpg"alt="logo" width="42" height="42"></a>
+		    <ul class="uk-navbar-nav uk-hidden-small">
+			    <ul class="uk-navbar-nav">
+			    	<li class="uk-active"><a href="">Home</a></li>
+		        	<li><a href="">Menu</a></li>
+		        	<li><a href="">About</a></li>
+		        	<li><a href="">Contact</a></li>
+		        </ul>
+		    </ul>
+		    <div class="uk-navbar-flip uk-hidden-small">
+		        <ul class="uk-navbar-nav">
+		            <li><a href=""><i class="uk-icon-facebook-official"></i></a></li>
+		        	<li><a href=""><i class="uk-icon-yelp"></i></a></li>
+		        </ul>
+    		</div>
+
+    		<a href="#offcanvas-nav" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
+		    <div class="uk-navbar-content uk-navbar-center uk-visible-small">
+		    	<a href="" class=""><img src="images/circlelogo.jpg"alt="logo" width="42" height="42"></a>
+		    </div> 
+		    
+	   
+		</div>
+	</nav>
+
+	<div id="offcanvas-nav" class="uk-offcanvas">
+		<div class="uk-offcanvas-bar">
+			<ul class="uk-navbar-nav uk-hidden-small">
+			    <ul class="uk-navbar-nav">
+			    	<li class="uk-active"><a href="">Home</a></li>
+		        	<li><a href="">Menu</a></li>
+		        	<li><a href="">About</a></li>
+		        	<li><a href="">Contact</a></li>
+		        </ul>
+		    </ul>
+	</div>
+
+	</div>
+	<div class="uk block uk-block-secondary">
+		Second
+	</div>
+	<!-- End of Header -->
+
+	<!-- Link to Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+	<!-- link to uikit js -->
+	<script type="text/javascript" src="/uikit/js/uikit.min.js"></script>
+</body>
+</html>
